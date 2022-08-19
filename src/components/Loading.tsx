1d5b25906e2b6723beb66bbe29ec371e5e72e75f
Ryan Iaro <98638811+RyanIaro@users.noreply.github.com>
2022-08-19 18:54:19 +0300

    Create Loading.tsx

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Loading():React.ReactElement {
+    return(
+        <div className="App">
+            <div className="Container">
+                <h1>Loading...</h1>
+            </div>
+        </div>
+    );
+}
